3b8ed996f009a228e169d622df647da8bbe7ee8f
Anakondos <Anakondos@gmail.com>
2022-10-13 02:34:09 -0400

    File creation commit

---
+++ Lesson_Math.js
@@ -0,0 +1,11 @@
+// Math methods, Stan Lesson, 
+
+let arr = [28, 16, 37, 21, 17, 27];
+//-----min, max -------
+console.log(Math.min(...arr));
+console.log(Math.max(...arr));
+
+// pow
+//Math.pow(num, power)
+console.log(Math.pow(2, 4))
+
