f3219157c8dda559e21b946ee7e1459827dc3279
Anakondos <Anakondos@gmail.com>
2022-10-17 23:05:33 -0400

    update

--- Lesson_Object.js
+++ Lesson_Object.js
@@ -1,6 +1,6 @@
 //Контейнер для набора свойсвтю состоит из набора ключей и значений (keys, value)
 //           key = string
-const obj = {a: 'a', b: 123, c: true, d: [1, 2, 3, 4]};
+// const obj = {a: 'a', b: 123, c: true, d: [1, 2, 3, 4]};
 
 // let newObj = new Object(); // - используеться с конструктором
 
@@ -21,5 +21,33 @@ const obj = {a: 'a', b: 123, c: true, d: [1, 2, 3, 4]};
 // Object.values();
 // Object.entries();
 
-console.log(Object.keys(newObj));
+// console.log(Object.keys(obj));
 
+//--------------boredom-----------------------
+let staff = {tim: 'change', jim: 'accounts',
+  randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
+  laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
+  mr: 'finance' };
+
+  function boredom(staff){
+  
+    let arr = {
+       accounts: 1,
+       finance: 2,
+       canteen: 10,
+       regulation: 3,
+       trading: 6,
+       change: 6,
+       IS: 8,
+       retail: 5,
+       cleaning: 4,
+       'pissing about': 25,
+    }
+    
+    let sum = Object.keys(staff).reduce((acc, el) => acc + arr[staff[el]],0 ); 
+     console.log(sum);
+    
+    return sum <= 80 ? 'kill me now' : sum < 100 ? 'i can handle this' : 'party time!!'; 
+   }
+
+   console.log(boredom(staff));
